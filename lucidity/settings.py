a9ccaa2c9795b1551e84523991c734c1c1196a09
Emily Foley <efoley326@gmail.com>
2021-08-07 13:46:44 -0400

    allowed host

--- lucidity/settings.py
+++ lucidity/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ["https://momentum-lucidity.herokuapp.com/",
+ALLOWED_HOSTS = [".herokuapp.com",
                 "127.0.0.1"
 ]
 

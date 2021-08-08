1afd4b636680f8f6cb332fd31ec1921c1b8cdf7f
Emily Foley <efoley326@gmail.com>
2021-08-07 22:10:12 -0400

    rest perm class

--- lucidity/settings.py
+++ lucidity/settings.py
@@ -20,6 +20,12 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
+
 ALLOWED_HOSTS = [".herokuapp.com"
 ]
 

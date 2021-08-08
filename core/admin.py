fafb71864d0faaabaa6821903e3c6d5d3a353ccf
Emily Foley <efoley326@gmail.com>
2021-08-07 22:02:06 -0400

    status bar and tag models

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Document, Event, Note, User, Alert, VolunteerSlot
+from .models import Document, Event, Note, StatusBar, Tag, User, Alert, VolunteerSlot
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Event)
@@ -7,3 +7,5 @@ admin.site.register(Document)
 admin.site.register(Alert)
 admin.site.register(Note)
 admin.site.register(VolunteerSlot)
+admin.site.register(StatusBar)
+admin.site.register(Tag)

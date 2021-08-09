3bcd7d5b6e5eb0096d8c95c0ca4dc6c0a3b6fff9
Emily Foley <efoley326@gmail.com>
2021-08-09 14:44:52 -0400

    djoser

--- core/views.py
+++ core/views.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
+from rest_framework import status,authentication, permissions, generics
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class VolunteerList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAdminUser]
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = UserSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+
+
--- lucidity/urls.py
+++ lucidity/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.conf import settings
 from django.urls import include, path
+from django.views.generic import ListCreateAPIView
+from django.contrib.auth.models import User
+from core.serializers import UserSerializer
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include ('rest_framework.urls')),
+    path('auth/', include('djoser.urls')),
+    path('users/', ListCreateAPIView.as_view(queryset=User.objects.all(), serializer_class=UserSerializer), name='volunteer-list'),
+
 ]

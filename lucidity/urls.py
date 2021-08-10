edf2861139d026397ede74227410326d3c36aa00
Emily Foley <efoley326@gmail.com>
2021-08-10 16:46:42 -0400

    took some urls

--- lucidity/urls.py
+++ lucidity/urls.py
@@ -7,6 +7,4 @@ urlpatterns = [
     path('api-auth/', include ('rest_framework.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
-    path('volunteers/', views.user_list),
-    path('volunteers/<int:pk>/', views.user_profile),
 ]

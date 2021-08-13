7a415f99b8f48acf86d18eb04bc211514e96e4f8
Emily Foley <efoley326@gmail.com>
2021-08-13 17:43:37 -0400

    removed authsession in settings

--- lucidity/settings.py
+++ lucidity/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.TokenAuthentication",
-        "rest_framework.authentication.SessionAuthentication"
     ),
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.AllowAny"

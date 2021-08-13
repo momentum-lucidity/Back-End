c7555985bcb696162d8ddea458caa590bb714c77
Emily Foley <efoley326@gmail.com>
2021-08-13 12:49:55 -0400

    model pks and default user in serializers

--- core/serializers.py
+++ core/serializers.py
@@ -12,7 +12,9 @@ class UserSerializer(serializers.ModelSerializer):
         'intake_status','preferred_event']
 
 class EventSerializer(serializers.ModelSerializer):
-
+    owner = serializers.HiddenField(
+    default=serializers.CurrentUserDefault()
+)
     class Meta:
         model = Event
         fields = ['user', 'title', 'date', 'start_time', 'end_time', 'type', 'description']
@@ -24,27 +26,36 @@ class UrlSerializer(serializers.HyperlinkedModelSerializer):
         fields = ['url']
 
 class DocumentSerializer(serializers.ModelSerializer):
+    owner = serializers.HiddenField(
+    default=serializers.CurrentUserDefault()
+)
     class Meta:
         model = Document
         fields = ['user', 'title', 'summary', 'body', 'url', 'required']
 
 
 class AlertSerializer(serializers.ModelSerializer):
-
+    owner = serializers.HiddenField(
+    default=serializers.CurrentUserDefault()
+)
     class Meta:
         model = Alert
         fields = ['user', 'title', 'date','text']
 
 
 class NoteSerializer(serializers.ModelSerializer):
-
+    owner = serializers.HiddenField(
+    default=serializers.CurrentUserDefault()
+)
     class Meta:
         model = Note
         fields = ['user','text']
 
 
 class VolunteerSlotSerializer(serializers.ModelSerializer):
-
+    owner = serializers.HiddenField(
+    default=serializers.CurrentUserDefault()
+)
     class Meta:
         model = VolunteerSlot
         fields = ['user', 'text_slot', 'event', 'time']
@@ -52,14 +63,18 @@ class VolunteerSlotSerializer(serializers.ModelSerializer):
 
 class StatusBarSerializer(serializers.ModelSerializer):
     required = serializers.ReadOnlyField(source='Document.required')
-
+    owner = serializers.HiddenField(
+    default=serializers.CurrentUserDefault()
+)
     class Meta:
         model = StatusBar
         fields = ['user', 'unfinished', 'pending', 'approved', 'complete']
 
 
 class TagSerializer(serializers.ModelSerializer):
-
+    owner = serializers.HiddenField(
+    default=serializers.CurrentUserDefault()
+)
     class Meta:
         model = Tag
         fields = ['user', 'text', 'event']

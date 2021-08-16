e34610b18dbe1e2c998e51bdda74984098370c97
itsdeebarnett <jdeebarnett@gmail.com>
2021-08-16 15:11:13 -0400

    pagination-page-size-adjustment

--- lucidity/settings.py
+++ lucidity/settings.py
@@ -26,7 +26,7 @@ DEBUG = True
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": 'rest_framework.pagination.PageNumberPagination',
-    "PAGE_SIZE":5,
+    "PAGE_SIZE":6,
 
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.TokenAuthentication",
